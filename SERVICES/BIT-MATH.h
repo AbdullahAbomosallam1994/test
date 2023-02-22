f275771e81d831f8092bb3e4388835d73f590c99
Abdullah Abomosallam <Abdullah.Abomosallam@coretech-innovations.com>
2023-02-22 16:18:30 +0200

    message 1

--- SERVICES/BIT-MATH.h
+++
@@ -1,5 +0,0 @@
-#define SET_BIT(REG,BIT)          (REG|=(1<<BIT))   
-#define CLR_BIT(REG,BIT)          (REG&= ~(1<<BIT))
-#define GET_BIT(REG,BIT)          ((REG>>BIT)&1)
-#define TOGGLE_BIT(REG,BIT)       (REG^=(1<<BIT))
-#define WRITE_BIT(REG,BIT,VALUE)  (REG = (VALUE<<BIT) | (REG&~(1<<BIT))  
